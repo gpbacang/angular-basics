684f5ff85327e1984fc4ad3d17f9168aecb429b1
Geraldine <geraldine@g-angle.co.jp>
2017-01-11 09:45:21 +0800

    Use ng-content

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -5,7 +5,15 @@ import { Component } from '@angular/core';
   template: `
       <h1>Inline Template</h1>
       <fa-other></fa-other>
-      <fa-another></fa-another>
+      <fa-another>
+        <div>
+          <h1>hello</h1>
+          <p>world!</p>
+        </div>
+      </fa-another>
+      <fa-another>
+        <p>something else!</p>
+      </fa-another>
     `,
   styles: [`
       h1{
