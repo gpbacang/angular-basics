573e27be30f7ccc91328f9f24304caced7ef0591
Geraldine <geraldine@g-angle.co.jp>
2017-01-11 10:05:45 +0800

    Use string interpolation

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,23 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <h1>Inline Template</h1>
-      <fa-other></fa-other>
-      <fa-another>
-        <div>
-          <h1>hello</h1>
-          <p>world!</p>
-        </div>
-      </fa-another>
-      <fa-another>
-        <p>something else!</p>
-      </fa-another>
-    `,
-  styles: [`
-      h1{
-        color: red;
-      }
-    `]
+      <h1>Root Component</h1>
+      <fa-databinding></fa-databinding>
+
+    `
 })
 export class AppComponent {
   title = 'i changed it!';
